1d6e848cb0df339dc33f5fc65d59113c35acc956
AnumitaDayal <60016030+AnumitaDayal@users.noreply.github.com>
2020-01-21 11:44:30 -0500

    First Code created during Class 2

---
+++ Class 2.py
@@ -0,0 +1,40 @@
+# this is sample code
+"""
+message = "Python version : 3.x"
+n = 17
+pi = 3.14159
+print (message)
+print (n)
+print (pi)
+print (type (message))
+print (type (n))
+print (type (pi))
+
+folder = "d:/data/ma/"
+file_1 = "roads.shp"
+road_layer = folder + file_1
+print (road_layer)
+"""
+""" my_string = "pointdata"
+print (my_string*3)
+"""
+
+"""
+n = 42
+print ("first line\n second line")
+"""
+"""
+str = 'Have you read \"Things Fall Apart\"?'
+print (str)
+"""
+"""
+pi = 3.14
+r = 2.0
+area = pi*(r**2)
+perim = 2.0*pi*r
+print "Given r:", r
+print "area is:", area
+print "Perimeter is: ", perim
+"""
+
+
